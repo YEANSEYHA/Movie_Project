5c7283a39559ccea89c5f685d862f058f05b9869
Yean Seyha <yeanseyha7@gmail.com>
2021-07-10 10:36:51 +0700

    [Task] Adding Nginx Configuration files nginx.conf

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,18 +1,13 @@
 import bcrypt from 'bcryptjs'
 
 const users =[
-   
+
     {
-        name: 'Admin ',
-        email: 'admin@gmail.com',
+        name: 'Admin1',
+        email: 'admin1@outlook.com',
         password: bcrypt.hashSync('123456',10),
         isAdmin: true
-    },
-    {
-        name: 'Admin ',
-        email: 'admin@outlook.com',
-        password: bcrypt.hashSync('123456',10),
-        isAdmin: true
+
     }
 ]
 
